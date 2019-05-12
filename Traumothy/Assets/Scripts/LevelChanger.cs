7fccb7a7235ebd6b74f7735d9660d5a53ae939f8
Very Gentil ami <salut@courriel.com>
2019-05-12 09:24:16 -0400

    changes

---
+++ Traumothy/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+	[SerializeField] public Animator animator;
+
+    private int levelToLoad;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            fadeToLevel(0);
+        }
+	}
+
+    public void fadeToLevel(int level) {
+        levelToLoad = level;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void onFadeComplete() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}

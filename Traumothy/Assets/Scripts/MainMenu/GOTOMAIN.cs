aec6a3d10d07d8b1578b2a7514c162370d11ca69
Daphne Binard <daphnebinard@hotmail.com>
2019-05-11 20:26:40 -0400

    huger flex

---
+++ Traumothy/Assets/Scripts/MainMenu/GOTOMAIN.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GOTOMAIN : MonoBehaviour {
+
+    public void LoadToMain()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+}
---
+++ Traumothy/Assets/Scripts/MainMenu/GOTOSCENE.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GOTOSCENE : MonoBehaviour {
+
+    public void LoadToScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
